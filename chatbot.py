5369821295902e6812e964ded4a90c0875fbecce
F <fwrizalman@gmail.com>
2017-04-02 11:42:57 +0800

    Add a basic bot

---
+++ chatbot.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from chatterbot import ChatBot
+
+__author__ = 'faezrah'
+
+# Uncomment the following line to enable verbose logging
+# logging.basicConfig(level=logging.INFO)
+
+bot = ChatBot(
+    'Norman',
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
+    storage_adapter='chatterbot.storage.JsonFileStorageAdapter',
+    input_adapter='chatterbot.input.TerminalAdapter',
+    output_adapter='chatterbot.output.TerminalAdapter',
+    logic_adapters=[
+        'chatterbot.logic.BestMatch',
+    ],
+    filters=[
+        'chatterbot.filters.RepetitiveResponseFilter'
+    ],
+    database='./database.json'
+)
+
+# Train based on the english corpus
+bot.train(
+    'chatterbot.corpus.english',
+    'chatterbot.corpus.english.greetings',
+    'chatterbot.corpus.english.conversations'
+)
+
+print('Type something to begin...')
+
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        # We pass None to this method because the parameter
+        # is not used by the TerminalAdapter
+        bot_input = bot.get_response(None)
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
